f512e4a1c4c93d680cf212a4eac6d23ef4af6e83
hyeminililo <ng040115@gmail.com>
2025-03-18 20:21:03 +0900

    #1 Design : 약관동의 UI 수정
    피그마 수정사항에 맞춰 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:keodam_frontend/signup/view/AgreementScreen.dart';
+import 'package:keodam_frontend/signup/view/agreement_screen.dart';
 
 void main() {
   runApp(const MyApp());
