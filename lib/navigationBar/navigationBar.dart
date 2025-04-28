aba3ce6f5523b5b7b0f9f81d85616f52cd65e918
choi-day <choisschoco@gmail.com>
2025-04-28 14:50:41 +0900

    #7 add: 이미지 파일, 네비게이션바 코드 파일 생성

---
+++ lib/navigationBar/navigationBar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class navigationBar extends StatefulWidget {
+  const navigationBar({super.key});
+
+  @override
+  State<navigationBar> createState() => _navigationBarState();
+}
+
+class _navigationBarState extends State<navigationBar> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
